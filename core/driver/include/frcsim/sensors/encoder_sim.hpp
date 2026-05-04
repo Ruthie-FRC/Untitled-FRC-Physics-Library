63b6d5ec23ced860b4f498f69ae5afca2da61161
Ruthie <s2169121@online.houstonisd.org>
2026-05-04 08:07:50 -0500

    feat: Add SpringForce, Arm, Elevator, EncoderSim, GyroSim, and VisionSim classes with basic functionality and documentation

--- core/driver/include/frcsim/sensors/encoder_sim.hpp
+++ core/driver/include/frcsim/sensors/encoder_sim.hpp
@@ -3,3 +3,30 @@
 // the LGPLv3 license file in the root directory of this project.
 
 #pragma once
+
+#include "frcsim/math/vector.hpp"
+
+namespace frcsim {
+
+/**
+ * @brief Simulates an absolute or relative encoder mapping to mechanical state.
+ */
+class EncoderSim {
+public:
+    EncoderSim() = default;
+    virtual ~EncoderSim() = default;
+
+    /**
+     * @brief Returns the simulated raw position or tick count.
+     * @return Simulated position.
+     */
+    virtual double GetPosition() const = 0;
+
+    /**
+     * @brief Returns the simulated velocity/rate.
+     * @return Simulated rate.
+     */
+    virtual double GetRate() const = 0;
+};
+
+} // namespace frcsim
--- core/driver/include/frcsim/sensors/gyro_sim.hpp
+++ core/driver/include/frcsim/sensors/gyro_sim.hpp
@@ -3,3 +3,31 @@
 // the LGPLv3 license file in the root directory of this project.
 
 #pragma once
+
+#include "frcsim/math/quaternion.hpp"
+#include "frcsim/math/vector.hpp"
+
+namespace frcsim {
+
+/**
+ * @brief Simulates an IMU/Gyroscope, incorporating drift and noise models.
+ */
+class GyroSim {
+public:
+    GyroSim() = default;
+    virtual ~GyroSim() = default;
+
+    /**
+     * @brief Gets simulated angular position (yaw, pitch, roll).
+     * @return Simulated Euler angles or rotation.
+     */
+    virtual Quaternion GetOrientation() const = 0;
+
+    /**
+     * @brief Gets rotational velocity vector.
+     * @return Radians per second for x, y, z axes.
+     */
+    virtual Vector3 GetAngularVelocity() const = 0;
+};
+
+} // namespace frcsim
