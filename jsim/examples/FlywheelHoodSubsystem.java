7815719583186148f38a471359c5c92d1f107751
Ruthie <s2169121@online.houstonisd.org>
2026-05-04 13:15:29 -0500

    feat: Implement FlywheelSubsystem and FlywheelHoodSubsystem with enhanced shooting capabilities and examples in RobotContainer

--- jsim/examples/FlywheelHoodSubsystem.java
+++ jsim/examples/FlywheelHoodSubsystem.java
@@ -1,4 +1,3 @@
-
 package examples;
 
 import api.GamepieceZone;
@@ -6,62 +5,10 @@ import api.SimRobot;
 import api.Rotation3d;
 
 /**
- * FlywheelSubsystem simulates a basic flywheel shooter with two motors/flywheels facing each other.
- * This is a common FRC design for launching balls.
- */
-public class FlywheelSubsystem {
-  private final GamepieceZone flywheelZone;
-  private double leftMotorVelocity = 0.0;
-  private double rightMotorVelocity = 0.0;
-  private Rotation3d exitAngle = new Rotation3d(0, 0, 0);
-
-  /**
-   * @param robot The simulated robot this subsystem is attached to.
-   */
-  public FlywheelSubsystem(SimRobot robot) {
-    this.flywheelZone = new GamepieceZone(robot);
-    this.flywheelZone.setMode(GamepieceZone.Mode.DISABLED);
-  }
-
-  /**
-   * Set the velocities of the left and right flywheel motors (in m/s).
-   * @param left Left flywheel velocity
-   * @param right Right flywheel velocity
-   * @param angle Exit angle for the shot
-   */
-  public void setFlywheel(double left, double right, Rotation3d angle) {
-    this.leftMotorVelocity = left;
-    this.rightMotorVelocity = right;
-    this.exitAngle = angle;
-  }
-
-  /**
-   * Fire a game piece using the current flywheel settings.
-   * The average of the two flywheel velocities is used for the exit speed.
-   */
-  public void shoot() {
-    double avgVelocity = (leftMotorVelocity + rightMotorVelocity) / 2.0;
-    flywheelZone.setExitParameters(avgVelocity, exitAngle);
-    flywheelZone.setMode(GamepieceZone.Mode.SHOOT);
-  }
-
-  /**
-   * Stop the flywheel shooter.
-   */
-  public void stop() {
-    flywheelZone.setMode(GamepieceZone.Mode.DISABLED);
-  }
-
-  public GamepieceZone getGamepieceZone() {
-    return flywheelZone;
-  }
-}
-
-/**
  * FlywheelHoodSubsystem simulates a flywheel shooter with a hood and a compression backspin roller.
  * This is a more advanced shooter that can impart backspin for higher arc shots.
  */
-class FlywheelHoodSubsystem {
+public class FlywheelHoodSubsystem {
   private final FlywheelSubsystem flywheel;
   private final GamepieceZone backspinRollerZone;
   private double backspinVelocity = 0.0;
---
+++ jsim/examples/RobotContainer.java
@@ -0,0 +1,45 @@
+package examples;
+
+import api.SimRobot;
+import api.Rotation3d;
+
+/**
+ * Example RobotContainer for wiring up FRC-style subsystems and commands.
+ * This class demonstrates how to instantiate and connect the FlywheelSubsystem and FlywheelHoodSubsystem
+ * for both real and simulated robot code.
+ */
+public class RobotContainer {
+    private final SimRobot robot;
+    private final FlywheelSubsystem flywheelSubsystem;
+    private final FlywheelHoodSubsystem flywheelHoodSubsystem;
+
+    public RobotContainer(SimRobot robot) {
+        this.robot = robot;
+        this.flywheelSubsystem = new FlywheelSubsystem(robot);
+        this.flywheelHoodSubsystem = new FlywheelHoodSubsystem(robot);
+    }
+
+    public void runFlywheelExample() {
+        // Example: Set flywheel to 15 m/s left, 15 m/s right, shoot at 30 degrees up
+        flywheelSubsystem.setFlywheel(15.0, 15.0, new Rotation3d(0, Math.toRadians(-30), 0));
+        flywheelSubsystem.shoot();
+        // ...simulate time passing, then stop
+        flywheelSubsystem.stop();
+    }
+
+    public void runHoodedShooterExample() {
+        // Example: Set flywheel to 18 m/s, backspin roller to 22 m/s, shoot at 45 degrees up
+        flywheelHoodSubsystem.setShot(18.0, 18.0, 22.0, new Rotation3d(0, Math.toRadians(-45), 0));
+        flywheelHoodSubsystem.shoot();
+        // ...simulate time passing, then stop
+        flywheelHoodSubsystem.stop();
+    }
+
+    public FlywheelSubsystem getFlywheelSubsystem() {
+        return flywheelSubsystem;
+    }
+
+    public FlywheelHoodSubsystem getFlywheelHoodSubsystem() {
+        return flywheelHoodSubsystem;
+    }
+}
